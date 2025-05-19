fc435292067154e6eb5651e4bfea4bcf7584c69b
amir <amirtaki623@gmail.com>
2025-05-19 19:43:48 +0330

    js : api & url & checkWeater

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // f5386d5abdb68c41a9946e1bbf35a438
+
+const apiKey = "f5386d5abdb68c41a9946e1bbf35a438"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unites=metric&q=bangalore";
+
+
+async function checkWeater(){
+    const respose =   await fetch(apiUrl + `&appid=${apiKey}`)
+    var data = await respose.json();
+
+    console.log(data)
+}
+checkWeater()
