507a4b41a2b8685041b783822a6626428fa3de48
amir <amirtaki623@gmail.com>
2025-05-19 20:06:16 +0330

    js : wearher

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@
 // f5386d5abdb68c41a9946e1bbf35a438
 
 const apiKey = "f5386d5abdb68c41a9946e1bbf35a438"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unites=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unites=imperial&q=";
 
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
 
-async function checkWeater(){
-    const respose =   await fetch(apiUrl + `&appid=${apiKey}`)
+async function checkWeater(city){
+    const respose =   await fetch(apiUrl + city + `&appid=${apiKey}`)
     var data = await respose.json();
-
 
     console.log(data)
     document.querySelector('.city').innerHTML = data.name
-    document.querySelector('.temp').innerHTML = data.main.temp + "°C"
+    document.querySelector('.temp').innerHTML = Math.round (data.main.temp) + "°C"
     document.querySelector('.humidity').innerHTML = data.main.humidity + "%"
     document.querySelector('.wind').innerHTML = data.wind.speed + "km/h"
 }
-checkWeater()
+
+searchBtn.addEventListener('click', (e)=>{ {
+    checkWeater(searchBox.value);
+}})
