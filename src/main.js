c5eca3fc4d6807c0c979fe5a781cb5355cc00b8b
amir <amirtaki623@gmail.com>
2025-05-19 20:13:45 +0330

    js : img weather

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?unites=imperial&
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+const weatherIcon =  document.querySelector('.weather-icon');
 
 async function checkWeater(city){
     const respose =   await fetch(apiUrl + city + `&appid=${apiKey}`)
@@ -16,6 +17,25 @@ async function checkWeater(city){
     document.querySelector('.temp').innerHTML = Math.round (data.main.temp) + "°C"
     document.querySelector('.humidity').innerHTML = data.main.humidity + "%"
     document.querySelector('.wind').innerHTML = data.wind.speed + "km/h"
+
+    if (data.weather[0].main == 'Clouds'){
+        weatherIcon.src  = 'src/image/clouds.png'
+    }
+    else if (data.weather[0].main == 'Rain'){
+        weatherIcon.src  = 'src/image/rain.png'
+    }
+    else if (data.weather[0].main == 'Clear'){
+        weatherIcon.src  = 'src/image/clear.png'
+    }
+    else if (data.weather[0].main == 'Drizzle'){
+        weatherIcon.src  = 'src/image/drizzle.png'
+    }
+    else if (data.weather[0].main == 'Mist'){
+        weatherIcon.src  = 'src/image/mist.png'
+    }
+
+
+
 }
 
 searchBtn.addEventListener('click', (e)=>{ {
